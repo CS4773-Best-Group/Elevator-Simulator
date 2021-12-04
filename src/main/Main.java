9d787880fdc82f9d02bb936d55cb15ef964363b6
Vanessa <hpj766@my.utsa.edu>
2021-12-04 14:48:52 -0600

    Add main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,24 @@
 package main;
 
+import colleagues.Door;
+import colleagues.Elevator;
+import mediator.ConcreteMediator;
+import mediator.Mediator;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: instantiate elevator
-        // TODO: make elevator calls
+        Elevator elevator = new Elevator();
+        Door door = new Door();
+
+        Mediator mediator = new ConcreteMediator();
+        mediator.registerElevator(elevator);
+        mediator.registerDoor(door);
+
+        elevator.press2();
+        elevator.press1();
+        elevator.press3();
+        elevator.press3();
+        elevator.press1();
     }
 }
